1017f57d33d8b3a7dc22d9081249644caa6909b9
simone-nolberto <simone.nolberto@outlook.com>
2024-05-30 17:38:54 +0200

    minor fix update

--- app/Http/Controllers/API/ProjectController.php
+++ app/Http/Controllers/API/ProjectController.php
@@ -9,7 +9,7 @@ class ProjectController extends Controller
 {
     public function index()
     {
-        $projects = Project::with('type', 'technologies')->paginate(2);
+        $projects = Project::with('type', 'technologies')->get();
 
         return response()->json([
 
